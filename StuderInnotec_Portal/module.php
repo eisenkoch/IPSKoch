7ac1682fd046cf6f2ebdaf037bcdf17e85797aae
eisenkoch <andreaskoch1@gmail.com>
2020-12-19 20:11:15 +0100

    added first translations added way to validate Variables

--- StuderInnotec_Portal/module.php
+++ StuderInnotec_Portal/module.php
@@ -9,9 +9,7 @@ class StuderInnotecWeb extends IPSModule {
     public function Create() {
         // Diese Zeile nicht löschen.
         parent::Create();
-		#ToDo: Archiv funktioniert noch nicht
-        //$archiv = IPS_GetInstanceIDByName("Archive", 0 );
-        
+		
         //Config Profile
         $this->RegisterProfileFloat("Studer-Innotec.MWh", 	"Factory", "", " MWh", 0, 0, 0, 3);
         $this->RegisterProfileFloat("Studer-Innotec.kWh", 	"Electricity", "", " kWh", 0, 0, 0, 2);
@@ -29,58 +27,63 @@ class StuderInnotecWeb extends IPSModule {
         $this->RegisterPropertyString('Password', '');
         $this->RegisterPropertyString("installationNumber", "");
         $this->RegisterPropertyString("url", "https://portal.studer-innotec.com/scomwebservice.asmx");
-        $this->RegisterTimer("UpdateTimer_5", 0, 'Studer_Update_5($_IPS[\'TARGET\']);');
+        $this->RegisterTimer("UpdateTimer_2", 0, 'Studer_Update_2($_IPS[\'TARGET\']);');
+		$this->RegisterTimer("UpdateTimer_5", 0, 'Studer_Update_5($_IPS[\'TARGET\']);');
 		$this->RegisterTimer("UpdateTimer_60", 0, 'Studer_Update_60($_IPS[\'TARGET\']);');
     }
-    public function Destroy() {
-        //Never delete this line!
-        parent::Destroy();
-    }
+public function Destroy() {
+    //Never delete this line!
+    parent::Destroy();
+}
 
-    public function ApplyChanges() {
-        // Diese Zeile nicht löschen
-        parent::ApplyChanges();
-		//clear Timer
-		$this->SetTimerInterval("UpdateTimer_5",0);
-		$this->SetTimerInterval("UpdateTimer_60",0);
-        if(empty($this->ReadPropertyString("Username"))){
-            //Warung fehlender Username
-			if ($this->ReadPropertyBoolean("Debug")){IPS_LogMessage($this->moduleName,"Warung fehlender Username");}
-            $this->SetStatus(201);
-		}else {$this->SetStatus(102);}
-		if(empty($this->ReadPropertyString("Password"))){
-            //Warung fehlendes Passwort
-			if ($this->ReadPropertyBoolean("Debug")){IPS_LogMessage($this->moduleName,"Warung fehlendes Passwort");}
-            $this->SetStatus(202);
-		}else {$this->SetStatus(102);}
-		if(empty($this->ReadPropertyString("installationNumber"))){
-            //Warung fehlende installationNumber
-            if ($this->ReadPropertyBoolean("Debug")){IPS_LogMessage($this->moduleName,"Warung fehlende installationNumber");}
-			$this->SetStatus(203);
-		}else {$this->SetStatus(102);}
-        
-		$treeData = json_decode($this->ReadPropertyString("Variables"));
-		if(!empty($treeData)){
-			$active = array();
-			foreach ($treeData as $value) {
-				if(($value->Active)==true){
-					$active[]=array('ID'=>($value->ID), 'Intervall'=>($value->Intervall));
-				}
-			}
-			$intervall_active = array_unique((array_column($active, 'Intervall')));
-			foreach ($intervall_active as $value) {
-				$this->SetTimerInterval(("UpdateTimer_".$value), $value*60000);
-				//$this->SetTimerInterval(("UpdateTimer_".$value), $value*1000);
+public function ApplyChanges() {
+	// Diese Zeile nicht löschen
+	parent::ApplyChanges();
+	//clear Timer
+	$this->SetTimerInterval("UpdateTimer_5",0);
+	$this->SetTimerInterval("UpdateTimer_60",0);
+	if(empty($this->ReadPropertyString("Username"))){
+		//Warung fehlender Username
+		if ($this->ReadPropertyBoolean("Debug")){IPS_LogMessage($this->moduleName,"Warung fehlender Username");}
+		$this->SetStatus(201);
+	}else {$this->SetStatus(102);}
+	if(empty($this->ReadPropertyString("Password"))){
+		//Warung fehlendes Passwort
+		if ($this->ReadPropertyBoolean("Debug")){IPS_LogMessage($this->moduleName,"Warung fehlendes Passwort");}
+		$this->SetStatus(202);
+	}else {$this->SetStatus(102);}
+	if(empty($this->ReadPropertyString("installationNumber"))){
+		//Warung fehlende installationNumber
+		if ($this->ReadPropertyBoolean("Debug")){IPS_LogMessage($this->moduleName,"Warung fehlende installationNumber");}
+		$this->SetStatus(203);
+	}else {$this->SetStatus(102);}
+	
+	$treeData = json_decode($this->ReadPropertyString("Variables"));
+	if(!empty($treeData)){
+		$active = array();
+		foreach ($treeData as $value) {
+			if(($value->Active)==true){
+				$active[]=array('ID'=>($value->ID), 'Intervall'=>($value->Intervall));
 			}
 		}
+		$intervall_active = array_unique((array_column($active, 'Intervall')));
+		foreach ($intervall_active as $value) {
+			$this->SetTimerInterval(("UpdateTimer_".$value), $value*60000);
+			//$this->SetTimerInterval(("UpdateTimer_".$value), $value*1000);
+		}
 	}
+}
     
 public function GetConfigurationForm(){
     $data = json_decode(file_get_contents(__DIR__ . "/form.json"), true);
 }
 
+public function Update_2() {
+	$timer_var = '2';
+	$this->call_Studer_from_Timer($timer_var);
+}
+
 public function Update_5() {
-	//IPS_LogMessage($this->moduleName,"Update_5");
 	$timer_var = '5';
 	$this->call_Studer_from_Timer($timer_var);
 }
@@ -184,21 +187,19 @@ private function Studer_Read($infoId,$paramart,$device) {
 private function RegisterProfileFloat($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits){
     #Fuction orignal from https://github.com/Joey-1970/
 	
-	        if (!IPS_VariableProfileExists($Name))
-	        {
-	            IPS_CreateVariableProfile($Name, 2);
-	        }
-	        else
-	        {
-	            $profile = IPS_GetVariableProfile($Name);
-	            if ($profile['ProfileType'] != 2)
-	                throw new Exception("Variable profile type does not match for profile " . $Name);
-	        }
-	        IPS_SetVariableProfileIcon($Name, $Icon);
-	        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
-	        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
-	        IPS_SetVariableProfileDigits($Name, $Digits);
-    }
+	    if (!IPS_VariableProfileExists($Name)) {
+	        IPS_CreateVariableProfile($Name, 2);
+	    }
+	    else {
+	        $profile = IPS_GetVariableProfile($Name);
+	        if ($profile['ProfileType'] != 2)
+	            throw new Exception("Variable profile type does not match for profile " . $Name);
+	    }
+	    IPS_SetVariableProfileIcon($Name, $Icon);
+	    IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+	    IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
+	    IPS_SetVariableProfileDigits($Name, $Digits);
+}
 
 public function validateAccount() {
 	global $email;
@@ -239,4 +240,42 @@ public function validateAccount() {
 		}
 	}
 }
+
+public function reCheckVar() {
+//function to check if a Variable what exists is active. if not active delete the Variable
+	$treeData = json_decode($this->ReadPropertyString("Variables"));
+	foreach ($treeData as $value) {
+		$var_ID = 'ID_' . $value->ID ;
+		if (@$this->GetIDForIdent($var_ID )) {
+			if (!$value->Active==true){
+				IPS_DeleteVariable($this->GetIDForIdent($var_ID ));
+				echo "deleted " . $var_ID . "\n";
+			}
+			if ($value->Active==true){
+				//
+				if (($value->Archive)==true){
+					AC_SetLoggingStatus($this->ReadPropertyInteger('ArchiveControlID'), $this->GetIDForIdent($var_ID), true);
+					IPS_ApplyChanges($this->ReadPropertyInteger('ArchiveControlID'));
+				}
+				else {
+					AC_SetLoggingStatus($this->ReadPropertyInteger('ArchiveControlID'), $this->GetIDForIdent($var_ID), false);
+					IPS_ApplyChanges($this->ReadPropertyInteger('ArchiveControlID'));
+					
+				}
+			}
+		}
+	}
+}
+
+public function CheckSofwareVersion() {
+	echo "this function is still in Development";
+	$infoId = "15078";
+	$type = "VS_Group";
+	//UIntValue FloatValue
+	$a = $this->Studer_Read($infoId,"Value",$type)->UIntValue;
+	echo $a;
+	echo ($a & 8). " " . ($a & 16). " " . ($a & 24). " " . ($a & 32);
+
+}
+
 }
